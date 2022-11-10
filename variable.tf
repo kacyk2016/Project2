adabb7330bed628356390477013006f8f95f7b52
kacyk2016 <94801373+kacyk2016@users.noreply.github.com>
2022-11-09 19:45:16 -0600

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,30 @@
+variable "name_input" {
+
+    description = "instance name hannah"
+
+    type = string
+
+    default = "hannah"
+
+    }
+
+
+variable "region" {
+
+    description = "region"
+
+    type = string
+
+    default = "us-east-2"
+
+    }
+
+
+
+variable "my_ami" {
+
+    description = "aws ami"
+
+    type = string
+
+    default = "ami-097a2df4ac947655f"
